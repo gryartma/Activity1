a7ae26778e78fcd7027ce36c40f79e1b1665673a
gryartma <64179342+gryartma@users.noreply.github.com>
2020-04-23 10:35:28 +0700

    Add files via upload

---
+++ pertemuan1/pertemuan1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan1
+{
+    class Class1
+    {
+        private double luas2, panjang2, lebar2;
+
+        public void Masukkan2()
+        {
+            Console.WriteLine("masukan Panjang2: ");
+            panjang2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("masukan lebar2: ");
+            lebar2 = Convert.ToDouble(Console.ReadLine());
+
+        }
+
+        public void Hitung2()
+        {
+            luas2 = panjang2 * lebar2;
+        }
+
+        public void Keluaran2()
+        {
+            Console.WriteLine("luas persegi panjang2 = {0}", luas2) ;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pertemuan1/pertemuan1/Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan1
+{
+    class Class2
+    {
+        double panjang3, lebar3;
+
+        public void Masukkan3()
+        {
+            Console.WriteLine("masukan Panjang3: ");
+            panjang3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("masukan lebar3: ");
+            lebar3 = Convert.ToDouble(Console.ReadLine());
+
+        }
+
+        public double Hitung3()
+        {
+            return panjang3 * lebar();
+        }
+
+        public void Keluaran3()
+        {
+            Console.WriteLine("luas persegi panjang3 = {0}", Hitung3());
+            Console.ReadKey();
+        }
+
+        public double lebar()
+        {
+            Console.WriteLine("masukan lebar :");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
